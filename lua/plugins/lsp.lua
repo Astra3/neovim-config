d1bd7acbb300540fbc24ea60e4ecc06143c6a046
Astra3 <rtaborsky02@gmail.com>
2024-11-08 23:00:43 +0100

    adding prettier for html, disabling treesitter indent

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,6 +23,7 @@ return {
                 -- You can customize some of the format options for the filetype (:help conform.format)
                 rust = { "rustfmt", lsp_format = "fallback" },
                 -- Conform will run the first available formatter
+                html = { "prettierd", "prettier", stop_after_first = true },
                 javascript = { "prettierd", "prettier", stop_after_first = true },
                 typescript = { "prettierd", "prettier", stop_after_first = true },
                 svelte = { "prettierd", "prettier", stop_after_first = true },
