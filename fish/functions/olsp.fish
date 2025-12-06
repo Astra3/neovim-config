4a5d1e97055f2b13548b4583a1acfc6dd734e668
Astra3 <rtaborsky02@gmail.com>
2025-12-06 01:14:42 +0100

    changing to llama.cpp

--- fish/functions/olsp.fish
+++ fish/functions/olsp.fish
@@ -1,21 +1,13 @@
 function olsp --description 'start ollama, start and open webui and then, after ctrl-c, stop ollama'
-    sudo systemctl start ollama
-    if test 0 -ne $status
-        echo "Could not start ollama properly"
-        return 1
-    end
-    echo "Started ollama"
+    llama-server --port 5000 &
+    echo "Started llama server"
     xdg-open localhost:8080 && WEBUI_AUTH=False DATA_DIR=~/.open-webui uvx --python 3.11 open-webui@latest serve
 
     if test 0 -ne $status
         echo "Open-WebUI did not exit cleanly!"
     end
 
-    sudo systemctl stop ollama
-    if test 0 -ne $status
-        echo "Could not stop ollama properly"
-        return 1
-    end
-    echo "Stopped ollama"
+    kill $(jobs --last --pid | grep '[0-9*]')
+    echo "Stopped llama server"
     return 0
 end
