6375a4c84f96999760f1ec730d63107b2c169307
Astra3 <rtaborsky02@gmail.com>
2026-02-20 22:39:33 +0100

    l stands for last

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -3,7 +3,7 @@ return {
     { "williamboman/mason.nvim",           opts = {} },
     { "williamboman/mason-lspconfig.nvim", opts = {} },
     { "windwp/nvim-ts-autotag",            opts = {} },
-    { "ggandor/leap.nvim",                 opts = {} },
+    { "https://codeberg.org/andyg/leap.nvim",                 opts = {} },
     { 'numToStr/Comment.nvim',             opts = {} },
     { "m4xshen/autoclose.nvim",            opts = {} },
     {
